21f6b0a6c7bf8c121368cbd7970f5773e8f498b1
Chu Hoang Son <chuson1996@gmail.com>
2016-10-23 20:27:44 +0300

    We don't need apis in this project

--- src/config.js
+++ src/config.js
@@ -12,8 +12,6 @@ const environment = {
 module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT,
-  apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIPORT,
   app: {
     title: 'My Social Media',
     description: 'All social media in one place.',
